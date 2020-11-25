50eccd7700ff51922e7caf97ec2ca932b667158f
gimenezes <menezes.s.gilda@gmail.com>
2020-11-25 06:10:20 -0300

    refactoring carousel

--- src/Components/CarouselCards/index.tsx
+++ src/Components/CarouselCards/index.tsx
@@ -1,56 +1,82 @@
 import React from "react";
 
-import {VscChevronRight, VscChevronLeft} from 'react-icons/vsc';
+import Carousel from "react-elastic-carousel";
 
 import Grant from "../../assets/grant.png";
 import Red from "../../assets/red.png";
 import Sybil from "../../assets/sybil.png";
 
-import {
-  Container,
-  Cards,
-  Card1,
-  GrantCard,
-  Card2,
-  RedCard,
-  Card3,
-  SybilCard,
-} from "./styles";
+import { Container, Item, PersonCard } from "./styles";
+
+import './carousel.css';
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1, pagination: false },
+  { width: 550, itemsToShow: 2, pagination: false },
+  { width: 768, itemsToShow: 3, pagination: false },
+];
 
 const CarouselCards: React.FC = () => {
   return (
     <Container>
-      <VscChevronLeft size={60} />
-      <Cards>
-        <Card1>
-          <GrantCard>
+      <Carousel breakPoints={breakPoints}>
+        <Item>
+          <PersonCard>
             <img src={Grant} alt="Grant" />
             <p>
               A Camerata foi apenas os dois no início, e suas fileiras nunca
               foram destinadas a exceder um número a ser contado em uma mão.
             </p>
-          </GrantCard>
-        </Card1>
-        <Card2>
-          <RedCard>
+          </PersonCard>
+        </Item>
+        <Item>
+          <PersonCard>
             <img src={Red} alt="Red" />
             <p>
               Red, uma jovem cantora, entrou em posse do Transistor. Sendo a
               poderosa espada falante. O grupo Possessores quer tanto ela quanto
               o Transistor e está perseguindo implacavelmente a sua procura.
             </p>
-          </RedCard>
-        </Card2>
-        <Card3>
-          <SybilCard>
+          </PersonCard>
+        </Item>
+        <Item>
+          <PersonCard>
             <img src={Sybil} alt="Sybil" />
             <p>
-            Sybil é descrita pelo Transistor como sendo os "olhos e ouvidos" da Camerata.
+              Sybil é descrita pelo Transistor como sendo os "olhos e ouvidos"
+              da Camerata.
             </p>
-          </SybilCard>
-        </Card3>
-      </Cards>
-      <VscChevronRight size={60} />
+          </PersonCard>
+        </Item>
+        <Item>
+          <PersonCard>
+            <img src={Grant} alt="Grant" />
+            <p>
+              A Camerata foi apenas os dois no início, e suas fileiras nunca
+              foram destinadas a exceder um número a ser contado em uma mão.
+            </p>
+          </PersonCard>
+        </Item>
+        <Item>
+          <PersonCard>
+            <img src={Red} alt="Red" />
+            <p>
+              Red, uma jovem cantora, entrou em posse do Transistor. Sendo a
+              poderosa espada falante. O grupo Possessores quer tanto ela quanto
+              o Transistor e está perseguindo implacavelmente a sua procura.
+            </p>
+          </PersonCard>
+        </Item>
+        <Item>
+          <PersonCard>
+            <img src={Sybil} alt="Sybil" />
+            <p>
+              Sybil é descrita pelo Transistor como sendo os "olhos e ouvidos"
+              da Camerata.
+            </p>
+          </PersonCard>
+        </Item>
+      </Carousel>
     </Container>
   );
 };
--- src/Components/CarouselCards/styles.ts
+++ src/Components/CarouselCards/styles.ts
@@ -2,56 +2,33 @@ import styled from 'styled-components';
 
 export const Container = styled.div `
 width: 100%;
-margin: 15% auto 10%;
+margin: 8% auto 10%;
 left: 0px;
-background: #363636 0% 0% no-repeat padding-box;
+background: #363636;
 opacity: 1;
 display: flex;
 justify-content: center;
 
-svg{
-    color: #FFF;
-    margin: 148px auto;
-    cursor: pointer;
-}
-
-@media(max-width: 768px){
-    margin: 40% auto 375px;
-
-    svg{
-        display: none;
-    }
-}
 
 @media(min-width: 1900px){
     margin: 10% auto 10%;
 }
-`;
-
-export const Cards = styled.div `
-display: flex;
-justify-content: center;
-gap: 5%;
-
-@media(max-width: 768px){
-    display: grid;
-    gap: 10%;
-}
 
 `;
 
-export const Card1 = styled.div `
+export const Item = styled.div `
 width: 300px;
-height: 435px;
-background: #FFFFFF 0% 0% no-repeat padding-box;
+height: 505px;
+background: #FFFFFF;
 box-shadow: 0px 3px 6px #FFFFFF63;
 border-radius: 27px;
 opacity: 0.8;
 display: flex;
 justify-content: center;
+padding: 10px 2px 5px;
 
 &:hover{
-    height: 440px;
+    height: 510px;
     width: 305px;
     opacity: 1;
     cursor: pointer;
@@ -62,11 +39,11 @@ justify-content: center;
 }
 `;
 
-export const GrantCard = styled.div `
+export const PersonCard = styled.div `
 width: 250px;
-margin: -65px auto 0px;
-height: 325px;
-background: #363636 0% 0% no-repeat padding-box;
+margin: -13px auto 0px;
+height: 335px;
+background: #363636;
 border-radius: 60px;
 border: 2px solid #FFFFFF;
 opacity: 1;
@@ -75,58 +52,9 @@ display: inline-grid;
 justify-content: center;
 
 img{
-    width: 274px;
-    height: 368px;
-    margin-top: -39px;
-}
-
-p{
-    font-size: 15px;
-    letter-spacing: 0px;
-    color: #363636;
-    opacity: 1;
-    padding: 10px 20px;
-}
-`;
-
-export const Card2 = styled.div `
-width: 300px;
-height: 435px;
-background: #FFFFFF 0% 0% no-repeat padding-box;
-box-shadow: 0px 3px 6px #FFFFFF63;
-border-radius: 27px;
-opacity: 0.8;
-display: flex;
-justify-content: center;
-
-&:hover{
-    height: 440px;
-    width: 305px;
-    opacity: 1;
-    cursor: pointer;
-}
-
-@media(max-width: 768px){
-    opacity: 1;
-}
-`;
-
-export const RedCard = styled.div `
-width: 250px;
-margin: -65px auto 0px;
-height: 325px;
-background: #363636 0% 0% no-repeat padding-box;
-border-radius: 60px;
-border: 2px solid #FFFFFF;
-opacity: 1;
-position: absolute;
-display: inline-grid;
-justify-content: center;
-
-img{
-    width: 274px;
-    height: 368px;
-    margin-top: -39px;
+    width: 275px;
+    height: 345px;
+    margin-top: -8px;
 }
 
 p{
@@ -139,51 +67,3 @@ p{
 `;
 
 
-export const Card3 = styled.div `
-width: 300px;
-height: 435px;
-background: #FFFFFF 0% 0% no-repeat padding-box;
-box-shadow: 0px 3px 6px #FFFFFF63;
-border-radius: 27px;
-opacity: 0.8;
-display: flex;
-justify-content: center;
-
-&:hover{
-    height: 440px;
-    width: 305px;
-    opacity: 1;
-    cursor: pointer;
-}
-
-@media(max-width: 768px){
-    opacity: 1;
-}
-`;
-
-export const SybilCard = styled.div `
-width: 250px;
-margin: -65px auto 0px;
-height: 325px;
-background: #363636 0% 0% no-repeat padding-box;
-border-radius: 60px;
-border: 2px solid #FFFFFF;
-opacity: 1;
-position: absolute;
-display: inline-grid;
-justify-content: center;
-
-img{
-    width: 274px;
-    height: 368px;
-    margin-top: -39px;
-}
-
-p{
-    font-size: 15px;
-    letter-spacing: 0px;
-    color: #363636;
-    opacity: 1;
-    padding: 10px 20px;
-}
-`;
